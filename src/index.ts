6bee16502f59027dddb896483f70fd5bc14f4b0a
Divya Sinha <sdivya@obopay.com>
2020-04-03 13:19:53 +0530

    first commit in dev branch

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export function main() {
   console.log('HELLO WORLD')
-  console.log('NEW VERSION CHANGE')
-  console.log('VERSION 1.0.2')
-  console.log('UPDATING BY MYSELF')
+  console.log('NEW BRANCH UPDATE')
+  console.log('VERSION 1.1.0')
 }
